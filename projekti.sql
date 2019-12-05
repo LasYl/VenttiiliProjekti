461db9522801c74733caf46a30c7846e14b7b4c1
LasYl <56217678+LasYl@users.noreply.github.com>
2019-12-05 18:48:15 +0200

    SQL-tiedosto toimivaksi

--- projekti.sql
+++ projekti.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 9.6.15
 -- Dumped by pg_dump version 9.6.15
 
--- Started on 2019-11-30 17:24:25
+-- Started on 2019-12-05 18:43:04
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -40,7 +40,7 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- TOC entry 185 (class 1259 OID 16451)
+-- TOC entry 187 (class 1259 OID 16597)
 -- Name: huollot; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -55,7 +55,7 @@ CREATE TABLE public.huollot (
 ALTER TABLE public.huollot OWNER TO postgres;
 
 --
--- TOC entry 186 (class 1259 OID 16454)
+-- TOC entry 188 (class 1259 OID 16619)
 -- Name: ostot; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -70,7 +70,7 @@ CREATE TABLE public.ostot (
 ALTER TABLE public.ostot OWNER TO postgres;
 
 --
--- TOC entry 187 (class 1259 OID 16457)
+-- TOC entry 185 (class 1259 OID 16575)
 -- Name: varastonimike; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -88,7 +88,7 @@ CREATE TABLE public.varastonimike (
 ALTER TABLE public.varastonimike OWNER TO postgres;
 
 --
--- TOC entry 188 (class 1259 OID 16463)
+-- TOC entry 186 (class 1259 OID 16583)
 -- Name: venttiili; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -108,78 +108,63 @@ CREATE TABLE public.venttiili (
 ALTER TABLE public.venttiili OWNER TO postgres;
 
 --
--- TOC entry 2147 (class 0 OID 16451)
--- Dependencies: 185
+-- TOC entry 2149 (class 0 OID 16597)
+-- Dependencies: 187
 -- Data for Name: huollot; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.huollot ("ID", "id_varastoNimike", id_venttiili, "PVM") FROM stdin;
-1	300001	40001	2018-11-13
-2	300006	40002	2019-07-12
-3	300002	40007	2019-07-12
-4	300009	40010	2019-07-12
-5	300008	40004	2019-08-19
-\.
+INSERT INTO public.huollot ("ID", "id_varastoNimike", id_venttiili, "PVM") VALUES (1, 300001, 40001, '2018-11-13');
 
 
 --
--- TOC entry 2148 (class 0 OID 16454)
--- Dependencies: 186
+-- TOC entry 2150 (class 0 OID 16619)
+-- Dependencies: 188
 -- Data for Name: ostot; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.ostot ("ID", "id_Varastonimike", "PVM", "Määrä") FROM stdin;
-1	300001	2018-12-29	2
-2	300004	2019-02-01	2
-3	300010	2019-05-04	2
-4	300007	2019-08-11	2
-5	300007	2019-08-22	1
-6	300001	2019-10-21	1
-\.
+INSERT INTO public.ostot ("ID", "id_Varastonimike", "PVM", "Määrä") VALUES (1, 300001, '2018-12-29', 2);
 
 
 --
--- TOC entry 2149 (class 0 OID 16457)
--- Dependencies: 187
+-- TOC entry 2147 (class 0 OID 16575)
+-- Dependencies: 185
 -- Data for Name: varastonimike; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.varastonimike (id, nimi, valmistaja, "myyjä", hinta, saldo, minimisaldo) FROM stdin;
-300001	LKB-F 25	Alfa-Laval	TetraPak	70	2	2
-300002	LKB-F 38	Alfa-Laval	TetraPak	95	2	2
-300003	LKB-F 51	Alfa-Laval	TetraPak	105	2	2
-300004	LKB-F 63	Alfa-Laval	TetraPak	120	2	2
-300005	Unique SSV 51	Alfa-Laval	TetraPak	95	2	2
-300006	Unique SSV 63	Alfa-Laval	TetraPak	105	2	2
-300007	Unique SSV 76	Alfa-Laval	TetraPak	120	2	2
-300008	SW43 51	SPX	Flow Serve	95	2	2
-300009	SW43 63	SPX	Flow Serve	105	2	2
-300010	SW43 76	SPX	Flow Serve	120	2	2
-\.
+INSERT INTO public.varastonimike (id, nimi, valmistaja, "myyjä", hinta, saldo, minimisaldo) VALUES (300001, 'LKB-F 25', 'Alfa-Laval', 'TetraPak', 70, 2, 2);
+INSERT INTO public.varastonimike (id, nimi, valmistaja, "myyjä", hinta, saldo, minimisaldo) VALUES (300002, 'LKB-F 38', 'Alfa-Laval', 'TetraPak', 95, 2, 2);
+INSERT INTO public.varastonimike (id, nimi, valmistaja, "myyjä", hinta, saldo, minimisaldo) VALUES (300003, 'LKB-F 51', 'Alfa-Laval', 'TetraPak', 105, 2, 2);
+INSERT INTO public.varastonimike (id, nimi, valmistaja, "myyjä", hinta, saldo, minimisaldo) VALUES (300004, 'LKB-F 63', 'Alfa-Laval', 'TetraPak', 120, 2, 2);
+INSERT INTO public.varastonimike (id, nimi, valmistaja, "myyjä", hinta, saldo, minimisaldo) VALUES (300005, 'Unique SSV 51', 'Alfa-Laval', 'TetraPak', 95, 2, 2);
+INSERT INTO public.varastonimike (id, nimi, valmistaja, "myyjä", hinta, saldo, minimisaldo) VALUES (300006, 'Unique SSV 63', 'Alfa-Laval', 'TetraPak', 105, 2, 2);
+INSERT INTO public.varastonimike (id, nimi, valmistaja, "myyjä", hinta, saldo, minimisaldo) VALUES (300007, 'Unique SSV 76', 'Alfa-Laval', 'TetraPak', 120, 2, 2);
+INSERT INTO public.varastonimike (id, nimi, valmistaja, "myyjä", hinta, saldo, minimisaldo) VALUES (300008, 'SW43 51', 'SPX', 'Flow Serve', 95, 2, 2);
+INSERT INTO public.varastonimike (id, nimi, valmistaja, "myyjä", hinta, saldo, minimisaldo) VALUES (300009, 'SW43 63', 'SPX', 'Flow Serve', 105, 2, 2);
+INSERT INTO public.varastonimike (id, nimi, valmistaja, "myyjä", hinta, saldo, minimisaldo) VALUES (300010, 'SW43 76', 'SPX', 'Flow Serve', 120, 2, 2);
+INSERT INTO public.varastonimike (id, nimi, valmistaja, "myyjä", hinta, saldo, minimisaldo) VALUES (300012, 'sdg', 'aadsg', '123', 23, 3, 3);
+INSERT INTO public.varastonimike (id, nimi, valmistaja, "myyjä", hinta, saldo, minimisaldo) VALUES (300011, 'sdfh', 'sdfh', 'sdfh', 34, 32, 34);
 
 
 --
--- TOC entry 2150 (class 0 OID 16463)
--- Dependencies: 188
+-- TOC entry 2148 (class 0 OID 16583)
+-- Dependencies: 186
 -- Data for Name: venttiili; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.venttiili (id, nimi, valmistaja, malli, koko, id_varastonimike, seuraava_huolto, huollettu, huoltovali_vuosina) FROM stdin;
-40003	Vesiventtiili	Alfa-Laval	LKB-F	63	300004	2021-03-18	2019-11-24	3
-40005	Vesiventtiili	Alfa-Laval	LKB-F	51	300003	2021-02-15	2019-11-24	3
-40008	Vesiventtiili	Alfa-Laval	Unique SSV	51	300005	2020-04-11	2019-11-24	3
-40009	Vesiventtiili	SPX	SW43	76	300010	2020-09-15	2019-11-24	3
-40001	Tuoteventtiili	Alfa-Laval	LKB-F	25	300001	2020-11-20	2019-11-24	3
-40002	Pesuventtiili	Alfa-Laval	Unique SSV	63	300006	2021-02-15	2019-11-24	3
-40004	Tuoteventtiili	SPX	SW43	51	300008	2021-02-15	2019-11-24	3
-40006	Tuoteventtiili	Alfa-Laval	Unique SSV	76	300007	2020-04-25	2019-11-24	3
-40007	Pesuventtiili	Alfa-Laval	LKB-F	38	300002	2020-02-28	2019-11-24	3
-40010	Pesuventtiili	SPX	SW43	63	300009	2020-02-03	2019-11-24	3
-\.
+INSERT INTO public.venttiili (id, nimi, valmistaja, malli, koko, id_varastonimike, seuraava_huolto, huollettu, huoltovali_vuosina) VALUES (40001, 'Vesiventtiili', 'Alfa-Laval', 'LKB-F', 25, 300001, '2020-11-20', '2019-11-24', 3);
+INSERT INTO public.venttiili (id, nimi, valmistaja, malli, koko, id_varastonimike, seuraava_huolto, huollettu, huoltovali_vuosina) VALUES (40003, 'Vesiventtiili', 'Alfa-Laval', 'LKB-F', 63, 300004, '2021-03-18', '2019-11-24', 3);
+INSERT INTO public.venttiili (id, nimi, valmistaja, malli, koko, id_varastonimike, seuraava_huolto, huollettu, huoltovali_vuosina) VALUES (40005, 'Vesiventtiili', 'Alfa-Laval', 'LKB-F', 51, 300003, '2021-02-15', '2019-11-24', 3);
+INSERT INTO public.venttiili (id, nimi, valmistaja, malli, koko, id_varastonimike, seuraava_huolto, huollettu, huoltovali_vuosina) VALUES (40007, 'Vesiventtiili', 'Alfa-Laval', 'LKB-F', 38, 300002, '2020-02-28', '2019-11-24', 3);
+INSERT INTO public.venttiili (id, nimi, valmistaja, malli, koko, id_varastonimike, seuraava_huolto, huollettu, huoltovali_vuosina) VALUES (40009, 'Vesiventtiili', 'SPX', 'SW43', 76, 300010, '2020-09-15', '2019-11-24', 3);
+INSERT INTO public.venttiili (id, nimi, valmistaja, malli, koko, id_varastonimike, seuraava_huolto, huollettu, huoltovali_vuosina) VALUES (40002, 'Tuoteventtiili', 'Alfa-Laval', 'Unique SSV', 63, 300006, '2021-02-15', '2019-11-24', 3);
+INSERT INTO public.venttiili (id, nimi, valmistaja, malli, koko, id_varastonimike, seuraava_huolto, huollettu, huoltovali_vuosina) VALUES (40004, 'Pesuventtiili', 'SPX', 'SW43', 51, 300008, '2021-02-15', '2019-11-24', 3);
+INSERT INTO public.venttiili (id, nimi, valmistaja, malli, koko, id_varastonimike, seuraava_huolto, huollettu, huoltovali_vuosina) VALUES (40006, 'Tuoteventtiili', 'Alfa-Laval', 'Unique SSV', 76, 300007, '2020-04-25', '2019-11-24', 3);
+INSERT INTO public.venttiili (id, nimi, valmistaja, malli, koko, id_varastonimike, seuraava_huolto, huollettu, huoltovali_vuosina) VALUES (40008, 'Tuoteventtiili', 'Alfa-Laval', 'Unique SSV', 51, 300005, '2020-04-11', '2019-11-24', 3);
+INSERT INTO public.venttiili (id, nimi, valmistaja, malli, koko, id_varastonimike, seuraava_huolto, huollettu, huoltovali_vuosina) VALUES (40010, 'Pesuventtiili', 'SPX', 'SW43', 63, 300009, '2020-02-03', '2019-11-24', 3);
 
 
 --
--- TOC entry 2014 (class 2606 OID 16470)
+-- TOC entry 2019 (class 2606 OID 16601)
 -- Name: huollot Huollot_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -188,7 +173,7 @@ ALTER TABLE ONLY public.huollot
 
 
 --
--- TOC entry 2018 (class 2606 OID 16472)
+-- TOC entry 2023 (class 2606 OID 16623)
 -- Name: ostot Ostot_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -197,7 +182,7 @@ ALTER TABLE ONLY public.ostot
 
 
 --
--- TOC entry 2023 (class 2606 OID 16474)
+-- TOC entry 2016 (class 2606 OID 16590)
 -- Name: venttiili Venttiili_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -206,7 +191,7 @@ ALTER TABLE ONLY public.venttiili
 
 
 --
--- TOC entry 2021 (class 2606 OID 16476)
+-- TOC entry 2014 (class 2606 OID 16582)
 -- Name: varastonimike varastoNimike_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -215,7 +200,7 @@ ALTER TABLE ONLY public.varastonimike
 
 
 --
--- TOC entry 2015 (class 1259 OID 16477)
+-- TOC entry 2020 (class 1259 OID 16618)
 -- Name: fki_Nimike; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -223,7 +208,7 @@ CREATE INDEX "fki_Nimike" ON public.huollot USING btree ("id_varastoNimike");
 
 
 --
--- TOC entry 2019 (class 1259 OID 16478)
+-- TOC entry 2024 (class 1259 OID 16629)
 -- Name: fki_VNimike; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -231,7 +216,7 @@ CREATE INDEX "fki_VNimike" ON public.ostot USING btree ("id_Varastonimike");
 
 
 --
--- TOC entry 2024 (class 1259 OID 16479)
+-- TOC entry 2017 (class 1259 OID 16596)
 -- Name: fki_Varastonimike; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -239,7 +224,7 @@ CREATE INDEX "fki_Varastonimike" ON public.venttiili USING btree (id_varastonimi
 
 
 --
--- TOC entry 2016 (class 1259 OID 16480)
+-- TOC entry 2021 (class 1259 OID 16607)
 -- Name: fki_venttiili; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -247,7 +232,7 @@ CREATE INDEX fki_venttiili ON public.huollot USING btree (id_venttiili);
 
 
 --
--- TOC entry 2025 (class 2606 OID 16481)
+-- TOC entry 2028 (class 2606 OID 16613)
 -- Name: huollot Nimike; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -256,7 +241,7 @@ ALTER TABLE ONLY public.huollot
 
 
 --
--- TOC entry 2028 (class 2606 OID 16486)
+-- TOC entry 2029 (class 2606 OID 16624)
 -- Name: ostot VNimike; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -265,7 +250,7 @@ ALTER TABLE ONLY public.ostot
 
 
 --
--- TOC entry 2029 (class 2606 OID 16491)
+-- TOC entry 2025 (class 2606 OID 16591)
 -- Name: venttiili Varastonimike; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -274,7 +259,7 @@ ALTER TABLE ONLY public.venttiili
 
 
 --
--- TOC entry 2026 (class 2606 OID 16496)
+-- TOC entry 2027 (class 2606 OID 16608)
 -- Name: huollot Varastonimike; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -283,7 +268,7 @@ ALTER TABLE ONLY public.huollot
 
 
 --
--- TOC entry 2027 (class 2606 OID 16501)
+-- TOC entry 2026 (class 2606 OID 16602)
 -- Name: huollot venttiili; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -291,9 +276,8 @@ ALTER TABLE ONLY public.huollot
     ADD CONSTRAINT venttiili FOREIGN KEY (id_venttiili) REFERENCES public.venttiili(id);
 
 
--- Completed on 2019-11-30 17:24:26
+-- Completed on 2019-12-05 18:43:05
 
 --
 -- PostgreSQL database dump complete
 --
-
